a0bea0698f7a3377a24b790b54689b965f73f805
Thierry Chau <thierry.chau@gmail.com>
2023-10-10 22:02:56 +0000

    refactoring update route with form_with model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -38,8 +38,8 @@ class MoviesController < ApplicationController
   def update
     @movie = Movie.find(params.fetch(:id))
 
-    @movie.title = params.fetch(:title)
-    @movie.description = params.fetch(:description)
+    movie_attributes = params.require(:movie).permit(:title, :description)
+    @movie.update(movie_attributes)
 
     if @movie.valid?
       @movie.save
