68c1f023a8b3b907c6dfa586afad7d7638392145
Thierry Chau <thierry.chau@gmail.com>
2023-10-10 21:55:59 +0000

    new form using form_with model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,9 +20,8 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.new
-    @movie.title = params.fetch(:movie).fetch(:title)
-    @movie.description = params.fetch(:movie).fetch(:description)
+    movie_attributes = params.require(:movie).permit(:title, :description)
+    @movie = Movie.new(movie_attributes)
 
     if @movie.valid?
       @movie.save
