16ef76ef17795bf49f4c0ed6e988543c4a39ac3e
Thierry Chau <thierry.chau@gmail.com>
2023-10-10 16:48:13 +0000

    refactored movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
   def new
     @the_movie = Movie.new
 
-    render template: "movies/new"
+    render "movies/new"
   end
 
   def index
@@ -16,7 +16,7 @@ class MoviesController < ApplicationController
       end
 
       format.html do
-        render  template: "movies/index"
+        render "movies/index"
       end
     end
   end
@@ -28,7 +28,7 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render template: "movies/show"
+    render "movies/show"
   end
 
   def create
@@ -40,7 +40,7 @@ class MoviesController < ApplicationController
       @the_movie.save
       redirect_to movies_url, notice: "Movie created successfully."
     else
-      render template: "movies/new"
+      render "movies/new"
     end
   end
 
@@ -51,7 +51,7 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render template: "movies/edit"
+    render "movies/edit"
   end
 
   def update
